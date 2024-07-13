81d2954a56ba7cd5173536b7197cd6c0df191505
Neil <xiehaoyico@gmail.com>
2024-07-14 03:49:15 +1000

    update0714

---
+++ contracts/solidity_by_example/018Modifier.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract ModifierExample  {
+
+    // 用户提款案例，需要验证2点：1.用户身份地址。 2.用户提款金额必须大于最小值
+    address public owner;
+    uint public minValue = 10;
+
+
+
+    constructor() {
+        owner = msg.sender; //获取发送交易者的地址
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "Only owner can call this function");
+        _; // 表示函数执行前和执行后的逻辑
+    }
+
+    modifier minValueCheck(uint _value) {
+        require(_value >= minValue, "Value must be greater than or equal to minValue");
+        _; // 表示函数执行前和执行后的逻辑
+    }
+
+// 直接将2条限制加在这个取款方法上即可使用modifier
+    function withdraw(uint amount) public onlyOwner minValueCheck(amount) {
+        // Only owner can withdraw and amount must be greater than or equal to minValue
+        // 实际的提款逻辑
+    }
+}
